ad31ab6e566a06aa3c683009acd5243bf0202e13
Eoin Mag Ualghairg <77682134+eoiniritium@users.noreply.github.com>
2021-03-29 01:58:21 +0100

    ;

---
+++ ML/Headerfiles/io.hpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+
+std::string read(std::string file){
+    std::ifstream f(file);
+    std::string output;
+    
+    f >> output;
+    return output;
+}
+
+void output(std::string file, std::string data){
+    std::ofstream f(file);
+
+    f << data;
+}
---
+++ ML/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
